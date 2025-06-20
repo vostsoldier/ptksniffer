c41dbbe7d7e52cfe8c9a78a67a39fb0c778d5f7e
Peter Park <fantasyindeath86@gmail.com>
2025-06-19 23:28:29 -0400

    bro

--- wifi-monitor/src/web/server.rs
+++ wifi-monitor/src/web/server.rs
@@ -1,22 +1,17 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use std::sync::{Arc, Mutex}; 
+use crate::web::handlers; 
 
 #[derive(Clone)]
 pub struct AppState {
     pub captured_data: Arc<Mutex<Vec<String>>>, 
 }
 
-async fn index(data: web::Data<AppState>) -> HttpResponse {
-    let captured_data = data.captured_data.lock().unwrap();
-    let response = captured_data.join("<br>");
-    HttpResponse::Ok().body(format!("Captured Data:<br>{}", response))
-}
-
 pub async fn start_server(state: AppState) -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(state.clone()))
-            .route("/", web::get().to(index))
+            .route("/", web::get().to(handlers::index)) 
     })
     .bind("0.0.0.0:8080")?  
     .run()
